f5f12bb735dc8744bc6c4b362f5fe21ab77b8e3a
Gabriel Martins <gcarvalhomartins23@gmail.com>
2024-10-30 16:26:54 -0400

    adicionando retorno no category service

--- backend/src/service/categoryService.js
+++ backend/src/service/categoryService.js
@@ -1,3 +1,5 @@
+import { json } from "express"
+
 const category = []
 
 function getAlltCategory(){
@@ -7,8 +9,10 @@ function getAlltCategory(){
 function addCategory(newCategoria){
     const {id_category,name_category} = newCategoria
     category.push({id_category,name_category})
-    return category
+    return { menssage:"Categoria criada com sucesso"}
 }
+
+
 
 
 export { 
