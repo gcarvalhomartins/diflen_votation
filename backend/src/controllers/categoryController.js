7ebc4848f96c9cd0bef9fd2a2ccad6d4f9cd01d2
Gabriel Martins <gcarvalhomartins23@gmail.com>
2025-01-17 16:43:49 -0400

    removendo verificacao if nas controllers de categoria

--- backend/src/controllers/categoryController.js
+++ backend/src/controllers/categoryController.js
@@ -3,43 +3,44 @@ import { getAlltCategory, addCategory, getCategoryId, updateCategory, deleteCate
 async function getCategoryHandler(req,res){
 
     try {
+
         const getCategoryServer = await getAlltCategory()
-
-        if(!getCategoryServer){
-            return res.status(404).json({message: "Erro ao buscar categorias"})
-        }
-
         return res.status(200).json(getCategoryServer)
+        
     }catch(error){
+
         console.error("Erro ao buscar categorias",error)
+        return res.status(404).json({message: "Categoria nao Encontrada "})
     }
 };
 
 async function addCategoryHandler(req,res){
-    const category = req.body;
+    try{
+        const category = req.body;
 
-    const new_category =  await addCategory(category)
+        const new_category =  await addCategory(category)
 
-    if(!new_category){
-        return res.status(400).json({message: "Erro ao adicionar categoria"})
+        return res.status(201).json(new_category)
+
+    }catch(error){
+        console.error("Erro ao adicionar categoria",error)
+        return res.status(400).json({message: "Erro ao adicionar categoria T_T"})
     }
-    return res.status(201).json(new_category)
+    
 }
 
 async function getCategoryBiId(req, res) {
     try {
+
         const id_categori = req.params.id;
         
-        if (!id_categori) {
-            return res.status(400).json({ message: "Id não informado" });
-        }
-        
         const category = await getCategoryId(id_categori);
+        
         return res.status(200).json(category);
         
     } catch (error) {
         console.error("Erro ao buscar categoria por id:", error.message);
-        return res.status(404).json({ message: error.message }); // Envia uma resposta ao cliente
+        return res.status(404).json({ message: "Categoria nao encontrada T_T" }); // Envia uma resposta ao cliente
     }
 }
 
@@ -47,9 +48,6 @@ async function updateCategoryHandler(req,res){
     try {
         const category = req.body;
 
-        if(!category || !category.id){
-            return res.status(400).json({message: "ID da categoria é obrigatório"})
-        }
         const result = await updateCategory(category)
 
         return res.status(200).json(result)
@@ -57,7 +55,7 @@ async function updateCategoryHandler(req,res){
     }catch(error){
         console.error("Erro ao atualizar categoria",error)
 
-        return res.status(404).json({message: error.message})
+        return res.status(404).json({message: "Categoria nao Encontrada T_T"})
     } 
     
 }
@@ -65,16 +63,12 @@ async function deleteCategoryHandler(req, res) {
     try {
         const category = req.body;
 
-        if (!category || !category.id) {
-            return res.status(400).json({ message: "ID da categoria é obrigatório" });
-        }
-
         const result = await deleteCategory(category);
         return res.status(200).json(result);
 
     } catch (error) {
         console.error("Erro ao deletar categoria:", error.message);
-        return res.status(404).json({ message: error.message });
+        return res.status(404).json({ message: "Categoria nao encontrada T_T" });
     }
 }
 
