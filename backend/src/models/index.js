644ff139bf60b04215e57590bcd683c79938b94c
Gabriel Martins <gcarvalhomartins23@gmail.com>
2025-01-28 15:16:19 -0400

    ajustando index.js dos models

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -2,7 +2,7 @@ import sequelize from "./database.js";
 import { category } from "./Category.js";
 import { candidato } from "./Candidato.js";
 
-// Defina as associações
+
 candidato.belongsTo(category, {
     foreignKey: "category_id",
     as: "categoria",
@@ -15,5 +15,5 @@ category.hasMany(candidato, {
 
 
 
-// Exporte os modelos associados
+
 export { category, candidato };
