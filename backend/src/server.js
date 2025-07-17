db1baa3e4cf61375625482fa66c0f01e6a4e20b1
Gabriel Martins <gcarvalhomartins23@gmail.com>
2025-07-17 16:52:27 -0400

    modify: modifi dockerfile and instaling lib postgress

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ const database = process.env.DATABASE
 const db_password = process.env.DB_PASSWORD
 console.log(host,db_password,db_port,db_user,database)
 
-app.listen( port|| 3333,()=>{
+app.listen( port || 3333,()=>{
     console.log(`Server ligado e funcionando na porta ${port}`)
 })
 
