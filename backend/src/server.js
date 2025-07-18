7852cba4b0896b0418bafabc72552e03e794d67f
Gabriel Martins <gcarvalhomartins23@gmail.com>
2025-07-18 09:31:11 -0400

    add: log

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,8 @@ const database = process.env.DATABASE
 const db_password = process.env.DB_PASSWORD
 console.log(host,db_password,db_port,db_user,database)
 
+console.log("cheguei no server")
+
 app.listen( port || 3333,()=>{
     console.log(`Server ligado e funcionando na porta ${port}`)
 })
